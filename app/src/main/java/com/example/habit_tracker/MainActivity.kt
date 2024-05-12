a391fe054cbdc14663b90743544cac126226a862
cmatrixgithub <cmatrixgithub@gmail.com>
2024-05-12 16:41:10 +0900

    Splash screen and app icon implemented

---
+++ app/src/main/java/com/example/habit_tracker/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.habit_tracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.habit_tracker.ui.theme.HabittrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition(
+            condition = { false }
+        )
+        Thread.sleep(200)
+        enableEdgeToEdge()
+        setContent {
+            HabittrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HabittrackerTheme {
+        Greeting("Android")
+    }
+}
